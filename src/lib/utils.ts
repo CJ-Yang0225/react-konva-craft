3d656e5327db5a92d1100ae508104f7a1bafc5e4
Jerry Yang <cj.yang0225@gmail.com>
2024-10-20 21:59:53 +0800

    refactor: change type Base64URLString to string

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,9 +6,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const encodeSvg = (
-  reactElement: React.ReactElement
-): Base64URLString => {
+export const encodeSvg = (reactElement: React.ReactElement): string => {
   // 將 React SVG 元素轉換為靜態 SVG 並進行 URL 編碼
   const svgString = ReactDOMServer.renderToStaticMarkup(reactElement);
   return 'data:image/svg+xml;base64,' + btoa(svgString);
